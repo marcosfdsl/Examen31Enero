f1d17c1bdec2e0478806c5822ed649c2e05bb4b6
Marcos Fernández de Salamanca López <116874613+marcosfdsl@users.noreply.github.com>
2022-12-27 21:27:21 +0100

    Modificación 5

--- src/mfds/MFdS.java
+++ src/mfds/MFdS.java
@@ -9,6 +9,8 @@ public class MFdS {
 		System.out.println("Modificación desde Master - Cambiado");
 		
 		System.out.println("Modificación desde Dev");
+		
+		System.out.println("Modificación desde GitHub");
 
 	}
 
