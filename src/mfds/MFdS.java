d4d8f39f7b73b141aa1d83599b3c1e791788abf9
MarcosFdS <marcos.fernandez@live.u-tad.com>
2022-12-27 20:31:52 +0100

    Modificación 2

--- src/mfds/MFdS.java
+++ src/mfds/MFdS.java
@@ -7,6 +7,8 @@ public class MFdS {
 		System.out.println("Alumno: Marcos Fernández de Salamanca");
 		
 		System.out.println("Primera Modificación desde Master");
+		
+		System.out.println("Modificación desde Dev");
 
 	}
 
